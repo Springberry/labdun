a2b4bc72f48448a708ff7a21242f274ada4a1eb7
Springberry <springberry@tutanota.com>
2023-03-12 15:09:01 +0100

    added super simple turn-based combat system

--- entities/player.py
+++ entities/player.py
@@ -1,4 +1,6 @@
 from entities.entity import Entity
 
 class Player(Entity):
-    pass
+    def __init__(self, x_position=0, y_position=0, symbol='@', health=10):
+        super().__init__(x_position, y_position, symbol)
+        self.health = health
--- main.py
+++ main.py
@@ -49,15 +49,31 @@ def move_player(new_x, new_y):
     global player, turn
     lookahead = level_map[new_y][new_x]
     if lookahead == obj_wall:
-        return
+        return False
     else:
         player.set_position(new_x, new_y)
         turn += 1
+        return True
 
 def check_for_enemy_encounter():
-    if turn % 20 == 0:
+    if turn % 20 == 0 and turn > 0:
         return True
     return False
+
+def battle_end_scene(stdscr, battle_end_status):
+    match battle_end_status:
+        case 1:
+            stdscr.addstr("\n\nYou won the battle!!")
+            stdscr.refresh()
+            time.sleep(1)
+        case 2:
+            stdscr.addstr("\n\nYou died")
+            stdscr.refresh()
+            time.sleep(1)
+        case 3:
+            stdscr.addstr("\n\nYou fled")
+            stdscr.refresh()
+            time.sleep(1)
 
 def enemy_encounter(stdscr):
     stdscr.clear()
@@ -67,35 +83,64 @@ def enemy_encounter(stdscr):
     stdscr.refresh()
     time.sleep(1)
 
+    enemy_health = 5
+    battle_end_status = 0
+
     while True:
         stdscr.erase()
         stdscr.addstr("Something attacks!!")
-        stdscr.addstr(1,0,"Attack [z]")
-        stdscr.addstr(2,0,"Flee [x]")
+        stdscr.addstr(f"\nYour health:{player.health}")
+        stdscr.addstr(f"\nEnemy health:{enemy_health}")
+        stdscr.addstr("\nAttack [z]")
+        stdscr.addstr("\nFlee [x]")
 
-        battle_end_status = 0
 
         stdscr.refresh()
 
-        try:
-            key = stdscr.getkey()
-        except:
-            continue
+        # Player Turn
+        while True:
+            try:
+                key = stdscr.getkey()
+            except:
+                continue
 
-        # Attack
-        if key == 'z':
-            stdscr.addstr("\n\nYou attacked!")
-            stdscr.refresh()
-            time.sleep(1)
-        # Flee
-        elif key == 'x':
-            battle_end_status = 2
+            # Attack
+            if key == 'z':
+                stdscr.addstr("\n\nYou attacked!")
+                damage = 1
+                enemy_health -= damage
+                stdscr.addstr(f"\nYou did {damage} damage")
+                stdscr.refresh()
+                time.sleep(1)
+
+                if enemy_health <= 0:
+                    battle_end_status = 1
+
+                break
+            # Flee
+            elif key == 'x':
+                battle_end_status = 3
+                break
+
+        if battle_end_status != 0:
+            battle_end_scene(stdscr, battle_end_status)
             break
 
-    if battle_end_status == 2:
-        stdscr.addstr("\n\nYou fled")
+        # Enemy Turn
+
+        stdscr.addstr("\n\nEnemy attacks you!")
+        enemy_damage = 1
+        player.health -= enemy_damage
+        stdscr.addstr(f"\nEnemy did {enemy_damage} damage")
         stdscr.refresh()
         time.sleep(1)
+
+        if player.health <=0:
+            battle_end_status = 2
+        
+        if battle_end_status != 0:
+            battle_end_scene(stdscr, battle_end_status)
+            break
 
 def game(stdscr):
     global player
@@ -113,16 +158,18 @@ def game(stdscr):
             key = stdscr.getkey()
         except:
             continue
-        if key == "w":
-            move_player(player.x_position, player.y_position - 1) 
-        elif key == "a":
-            move_player(player.x_position - 1, player.y_position) 
-        elif key == "s":
-            move_player(player.x_position, player.y_position + 1) 
-        elif key == "d":
-            move_player(player.x_position + 1, player.y_position) 
 
-        if check_for_enemy_encounter():
+        moved = False
+        if key == "w":
+            moved = move_player(player.x_position, player.y_position - 1) 
+        elif key == "a":
+            moved =move_player(player.x_position - 1, player.y_position) 
+        elif key == "s":
+            moved =move_player(player.x_position, player.y_position + 1) 
+        elif key == "d":
+            moved =move_player(player.x_position + 1, player.y_position) 
+
+        if check_for_enemy_encounter() and moved:
             enemy_encounter(stdscr)
 
     stdscr.refresh()
